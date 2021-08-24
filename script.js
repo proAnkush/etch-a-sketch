afbdb24bcfe28bf2668ae02ae92c4d9c844c13de
Ankush Patil <proankush69@gmail.com>
2021-08-24 12:33:11 +0530

    Added Buttons

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var gridSize = parseInt(prompt("Enter Grid size: "));
-console.log(gridSize);
-initiate();
+
+initiate(10);
 var usedColor = "black";
 
-function initiate(){
-    if(!Number(gridSize)){
+function initiate(gridSize){
+    if(!Number(gridSize) || gridSize < 0 || gridSize > 100 || gridSize == undefined){
         var gridSize = 10;
         console.log("Setting grid size to 10");
     }
@@ -17,9 +16,61 @@ function initiate(){
         drawableGrid.insertAdjacentElement('beforeend', gridSquare);
     }
     let gridPixels = drawableGrid.querySelectorAll("div");
+    gridPixels.forEach(gridP => gridP.style.backgroundColor = "white");
     gridPixels.forEach(gridP => gridP.addEventListener('mouseover', pixelFillColor));
 }
 
 function pixelFillColor(){
-    this.style.backgroundColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+    if(randomActive == true){
+        this.style.backgroundColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+    }else if(blackActive){
+        this.style.backgroundColor = "black";
+    }else if(eraserActive){
+        this.style.backgroundColor = "white";
+    }
+
 }
+function clear(){
+    console.log("hello");
+}
+
+
+var clearButton = document.getElementById("clear");
+var blackButton = document.getElementById("black");
+var randomButton = document.getElementById("random");
+var eraserButton = document.getElementById("eraser");
+var blackActive = false;
+var randomActive = true;
+var eraserActive = false;
+
+clearButton.addEventListener("click", function(){
+
+    initiate(parseInt(prompt("enter grid size: ")));
+})
+blackButton.addEventListener("click", function(){
+    blackButton.style.border = "2px solid yellow";
+    randomButton.style.border = "0px";
+    eraserButton.style.border = "0px";
+    blackActive = true;
+    randomActive = false;
+    eraserActive = false;
+    clearButton.style.border = "0px"
+})
+randomButton.addEventListener("click", function(){
+    randomButton.style.border = "2px solid yellow";
+    blackButton.style.border = "0px";
+    eraserButton.style.border = "0px";
+    randomActive = true;
+    blackActive = false;
+    eraserActive = false;
+    clearButton.style.border = "0px"
+})
+eraserButton.addEventListener("click", function(){
+    eraserButton.style.border = "2px solid yellow";
+    blackButton.style.border = "0px";
+    randomButton.style.border = "0px";
+    eraserActive = true;
+    randomActive = false;
+    blackActive = false;
+    clearButton.style.border = "0px"
+})
